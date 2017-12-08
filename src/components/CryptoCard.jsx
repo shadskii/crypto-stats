63257c31672526db3a2218e7005bb62c139a076a
shadskii <jake.k.hassel@gmail.com>
2017-12-08 15:08:04 -0500

    Styling change for prices deltas

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Card, CardHeader } from 'material-ui/Card';
 import Humanize from 'humanize-plus';
 import '../styles/CryptoCard.css';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
 
 export function CryptoCard(props) {
     return (
@@ -15,35 +23,78 @@ export function CryptoCard(props) {
                 subtitle={'Market Cap: $' + Humanize.formatNumber(props.info.market_cap_usd, 2)}
             >
                 <h5>Price: {'$' + props.info.price_usd}</h5>
-                <ul>
-                    <PercentChange
-                        desc={'1 hour'}
-                        price={props.info.price_usd}
-                        change={props.info.percent_change_1h}
-                    />
-                    <PercentChange
-                        desc={'24 hours'}
-                        price={props.info.price_usd}
-                        change={props.info.percent_change_24h}
-                    />
-                    <PercentChange
-                        desc={'7 days'}
-                        price={props.info.price_usd}
-                        change={props.info.percent_change_7d}
-                    />
-                </ul>
+                <Table style={{ width: '100%' }}>
+                    <TableHeader
+                        adjustForCheckbox={false}
+                        displaySelectAll={false}
+                        style={{ height: '10px' }}
+                    >
+                        <TableRow
+                            style={{ height: '10px' }}
+                        >
+                            <PriceLabel
+                                desc={'1hr'}
+                                change={props.info.percent_change_1h}
+                            />
+                            <PriceLabel
+                                desc={'24hr'}
+                                change={props.info.percent_change_24h}
+                            />
+                            <PriceLabel
+                                desc={'7d'}
+                                change={props.info.percent_change_7d}
+                            />
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody
+                        displayRowCheckbox={false}
+                    >
+                        <TableRow style={{ height: '10px' }}>
+                            <PriceChange
+                                price={props.info.price_usd}
+                                change={props.info.percent_change_1h}
+                            />
+                            <PriceChange
+                                price={props.info.price_usd}
+                                change={props.info.percent_change_24h}
+                            />
+                            <PriceChange
+                                price={props.info.price_usd}
+                                change={props.info.percent_change_7d}
+                            />
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </CardHeader>
         </Card>
+    );
+}
+
+function PriceChange(props) {
+    var delta = (Number(props.price) * Number(props.change)) / 100;
+    return (
+        <TableRowColumn style={{ height: '20px' }}>
+            {'$' + Humanize.formatNumber(delta, 2)}
+        </TableRowColumn>
     );
 }
 
 function PercentChange(props) {
     var delta = (Number(props.price) * Number(props.change)) / 100;
     return (
-        <li>
-            {props.desc + " "}
+        <TableRowColumn>
             <i className={delta > 0 ? "fa fa-caret-up up fa-lg" : "fa fa-caret-down down fa-lg"} aria-hidden="true"></i>
             {" " + props.change + '% ($' + Humanize.formatNumber(delta, 2) + ')'}
-        </li>
-    )
+        </TableRowColumn>
+    );
+}
+
+function PriceLabel(props) {
+    return (
+        <TableHeaderColumn
+            style={{ height: '10px' }}>
+            <i className={props.change > 0.0 ? "fa fa-caret-up up fa-lg" : "fa fa-caret-down down fa-lg"} aria-hidden="true"></i>
+            {' ' + props.desc + ' (' + Humanize.formatNumber(props.change, 2) + '%)'}
+        </TableHeaderColumn>
+    );
 }
