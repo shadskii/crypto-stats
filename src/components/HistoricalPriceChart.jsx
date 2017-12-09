89471f4455723df6f74cfa0ca6e8a23f88f76dbd
shadskii <jake.k.hassel@gmail.com>
2017-12-09 15:14:17 -0500

    Added buttons for graphs

--- src/components/HistoricalPriceChart.jsx
+++ src/components/HistoricalPriceChart.jsx
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 import moment from 'moment';
 import { historicalHourEndPoint } from '../constants';
+import FlatButton from 'material-ui/FlatButton';
 
+import FontIcon from 'material-ui/FontIcon';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+
+const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
+const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
+const nearbyIcon = <IconLocationOn />;
 
 export default class HistoricalPriceChart extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            graphSelect: 0,
             graphData: {
                 pricePoints: [],
                 labels: []
@@ -17,7 +27,6 @@ export default class HistoricalPriceChart extends Component {
     }
     refresh() {
         var _this = this;
-        console.log(this.props.symbol);
         fetch(historicalHourEndPoint(this.props.symbol))
             .then(result => result.json())
             .then(items => {
@@ -41,6 +50,7 @@ export default class HistoricalPriceChart extends Component {
         this.refresh();
     }
 
+    select = (index) => this.setState({ graphSelect: index });
     render() {
         var data = {
             labels: this.state.graphData.labels,
@@ -64,7 +74,8 @@ export default class HistoricalPriceChart extends Component {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: this.state.graphData.pricePoints
+                    data: this.state.graphData.pricePoints,
+                    cubicInterpolationMode: 'monotone'
                 }
             ]
         };
@@ -72,6 +83,9 @@ export default class HistoricalPriceChart extends Component {
         return (
             <div>
                 <Line data={data} legend={{ display: false }} />
+                <FlatButton label="1 Hour" primary={this.state.graphSelect === 0} onClick={() => this.select(0)} />
+                <FlatButton label="24 Hour" primary={this.state.graphSelect === 1} onClick={() => this.select(1)} />
+                <FlatButton label="7 Day" primary={this.state.graphSelect === 2} onClick={() => this.select(2)} />
             </div>
         );
     }
