960f7b3b77e0db984d82b4fcf679264e05478044
shadskii <jake.k.hassel@gmail.com>
2018-02-14 07:29:15 -0500

    BottomNave selection works

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -4,7 +4,7 @@ import FontIcon from 'material-ui/FontIcon';
 import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 
 const BottomNav = withRouter(({ history, location }) =>
-    <BottomNavigation selectedIndex={location.pathname === '/news' ? 1 : 0}>
+    <BottomNavigation selectedIndex={selectedIndex(location.pathname)}>
         <BottomNavigationItem
             label="Prices"
             icon={<FontIcon className="fa fa-line-chart" />}
@@ -23,4 +23,13 @@ const BottomNav = withRouter(({ history, location }) =>
     </BottomNavigation>
 );
 
+function selectedIndex(locationName) {
+    if (locationName === '/news') {
+        return 2;
+    } else if (locationName === '/favorites') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 export default BottomNav;
