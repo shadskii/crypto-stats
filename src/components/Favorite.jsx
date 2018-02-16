89146761bb10db38c453fe1634cb679acdbff92b
shadskii <jake.k.hassel@gmail.com>
2018-02-15 22:57:42 -0500

    Added favorite to favorite page

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { CryptoCard } from './CryptoCard';
+import CircularProgress from 'material-ui/CircularProgress';
+
+const API = "https://api.coinmarketcap.com/v1/ticker/";
+class Favorite extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            coin: [],
+            fetchingData: true
+        };
+    }
+
+    load() {
+        var _this = this;
+        fetch(API + this.props.coinId + '/')
+            .then(result => result.json())
+            .then(items => {
+                _this.setState({
+                    coin: items,
+                    fetchingData: false
+                });
+            })
+    }
+
+    componentDidMount() {
+        this.load();
+    }
+    render() {
+        return (
+
+            <div className='row'>
+                {this.state.fetchingData ?
+                    (<div className='center-content'>
+                        <CircularProgress size={80} thickness={7} />
+                    </div>)
+                    :
+                    (this.state.coin.map(function (el, index) {
+                        return <div key={index} className='col-md-6'>
+                            <CryptoCard info={el} />
+                        </div>
+                    }))
+                }
+            </div>
+        );
+    }
+}
+export default Favorite;
--- src/components/FavoritesPage.jsx
+++ src/components/FavoritesPage.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import AddFavoriteDialog from './AddFavoriteDialog';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import Favorite from './Favorite';
+import CircularProgress from 'material-ui/CircularProgress';
 const style = {
     position: 'fixed',
     bottom: 80,
@@ -8,10 +14,57 @@ const style = {
 };
 
 class FavoritesPage extends Component {
-
+    state = {
+        open: false,
+        coins: ['bitcoin']
+    };
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+    handleClose = () => {
+        this.setState({ open: false });
+    };
     render() {
+        const actions = [
+            <TextField
+                hintText="Coin Name"
+            />,
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Add"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+        ];
         return <div >
-            <FloatingActionButton style={style}>
+            <div className='container-fluid content-scroll'>
+                <div className='row'>
+                    {this.state.fetchingData ?
+                        (<div className='center-content'>
+                            <CircularProgress size={80} thickness={7} />
+                        </div>)
+                        :
+                        (this.state.coins.map(function (el, index) {
+                            return <div key={index} className='col-md-6'>
+                                <Favorite coinId={el} />
+                            </div>
+                        }))
+                    }
+                </div>
+            </div>
+            <Dialog
+                title="Add Favorite"
+                actions={actions}
+                modal={true}
+                open={this.state.open}
+            >
+                Add any coin that you want to track!
+        </Dialog>
+            <FloatingActionButton style={style} onClick={this.handleOpen}>
                 <ContentAdd />
             </FloatingActionButton>
         </div>
