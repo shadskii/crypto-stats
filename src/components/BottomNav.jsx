6a945d3ebf43cb3b2b235090e5c81b1969cdfbc0
shadskii <jake.k.hassel@gmail.com>
2018-03-11 14:55:15 -0400

    Bottom nav use redux

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom'
 import FontIcon from 'material-ui/FontIcon';
 import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import * as viewsConst from '../constants/Views'
+import PropTypes from 'prop-types'
 
-const BottomNav = withRouter(({ history, location }) =>
-    <BottomNavigation selectedIndex={selectedIndex(location.pathname)}>
+const BottomNav = ({ view, changeView }) => (
+    <BottomNavigation selectedIndex={selectedIndex(view.view)}>
         <BottomNavigationItem
             label="Prices"
             icon={<FontIcon className="fa fa-line-chart" />}
-            onClick={() => { history.push('/') }}
+            onClick={() => changeView(viewsConst.PRICE_PAGE)}
         />
         <BottomNavigationItem
             label="Favorites"
             icon={<FontIcon className="fa fa-heart" />}
-            onClick={() => { history.push('/favorites') }}
+            onClick={() => changeView(viewsConst.FAVORITE_PAGE)}
         />
         <BottomNavigationItem
             label="News"
             icon={<FontIcon className="fa fa-newspaper-o" />}
-            onClick={() => { history.push('/news') }}
+            onClick={() => changeView(viewsConst.NEWS_PAGE)}
         />
     </BottomNavigation>
 );
-
-function selectedIndex(locationName) {
-    if (locationName === '/news') {
+BottomNav.propTypes = {
+    view: PropTypes.object.isRequired,
+    changeView: PropTypes.func.isRequired
+}
+function selectedIndex(viewName) {
+    if (viewName === viewsConst.NEWS_PAGE) {
         return 2;
-    } else if (locationName === '/favorites') {
+    } else if (viewName === viewsConst.FAVORITE_PAGE) {
         return 1;
     } else {
         return 0;
