8c77743727b2f7d5d1c50e94ebd3c40268ecbe94
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-04 13:38:55 -0500

    Updates

---
+++ precache-manifest.0602f100a90f41f6fec48379516044c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c969cc8a854e8bbbb82c07eb11a4ff0",
+    "url": "/crypto-stats/static/media/reddit_icon.2c969cc8.png"
+  },
+  {
+    "revision": "5fae64fa29372e7e50a2506db7d5168e",
+    "url": "/crypto-stats/static/media/link_icon.5fae64fa.png"
+  },
+  {
+    "revision": "2a265a74ea5000969b93",
+    "url": "/crypto-stats/static/js/runtime~main.2a265a74.js"
+  },
+  {
+    "revision": "109f7af21ee2525d4e1e",
+    "url": "/crypto-stats/static/js/main.109f7af2.chunk.js"
+  },
+  {
+    "revision": "5556499a2647236b46d5",
+    "url": "/crypto-stats/static/js/1.5556499a.chunk.js"
+  },
+  {
+    "revision": "109f7af21ee2525d4e1e",
+    "url": "/crypto-stats/static/css/main.23ddc822.chunk.css"
+  },
+  {
+    "revision": "3d3c35d98fbff4f3e57432b9d6df458a",
+    "url": "/crypto-stats/index.html"
+  }
+];
